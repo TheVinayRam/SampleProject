9ba978a5742cad480bfe6ca5bcb4d96f6fe1e809
TheVinayRam <vinayram.ballapuram9@gmail.com>
2022-10-16 04:22:42 +0530

    First Code

---
+++ Java.java
@@ -0,0 +1,11 @@
+package fullstackjava;
+
+public class Java {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Eclipse IDE....");
+		System.out.println("This is Vinay Ram The java developer");
+
+	}
+
+}
